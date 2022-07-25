4911f256e2b3a972391cbb91d7a3445d0c3e3678
kavdesilva <97710440+kavdesilva@users.noreply.github.com>
2022-07-25 12:32:06 -0400

    Update and rename user.js to books.js

---
+++ books.js
@@ -0,0 +1,12 @@
+const { Schema } = require('mongoose')
+
+const Book = new Schema(
+    {
+        title: { type: String, required: true },
+        datePublished: { type: String, required: true },
+        description: { type: String, required: true },
+        genre: [{ type: String, required: true }], 
+    }
+)
+
+module.exports = Book
