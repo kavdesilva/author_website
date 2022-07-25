a97ab906ee1925a93d7f8194ee63d607ca47e2b0
kavdesilva <97710440+kavdesilva@users.noreply.github.com>
2022-07-25 14:25:42 -0400

    Update users.js

--- seed/users.js
+++ seed/users.js
@@ -1,1 +1,30 @@
+const db = require('../db')
+const { User, Book } = require('../models')
 
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
+const main = async () => {
+  const users = [
+    {
+      userName: '',
+      password: '',
+      bookshelf: [
+        {
+          favBooks: [book[0]._id, book[1]._id, book[2]._id] // array goes here
+        },
+        {
+          wantBooks: [book[3]._id, book[4]._id] // array goes here
+        }
+      ]
+  ]
+
+  await User.insertMany(users)
+  console.log('created users')
+}
+
+const run = async () => {
+  await main()
+  db.close()
+}
+
+run()
