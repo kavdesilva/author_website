5660d1bed8a69987ef5f16ef7d6d88d835efcde5
kavdesilva <97710440+kavdesilva@users.noreply.github.com>
2022-07-27 13:41:46 -0400

    Update user.js

--- models/user.js
+++ models/user.js
@@ -4,13 +4,10 @@ const User = new Schema(
     {
         userName: { type: String, required: true },
         password: { type: String, required: true },
+        email: { type: String, required: true },
         bookshelf: [
-          {
-            favBooks: [{ type: Schema.Types.ObjectId, ref: 'Book'}] // array goes here
-          },
-          {
-            wantBooks: [{ type: Schema.Types.ObjectId, ref: 'Book'}] // array goes here
-          }
+          { favBooks: [{ type: Schema.Types.ObjectId, ref: 'Book'}] }, // array goes here
+          { wantBooks: [{ type: Schema.Types.ObjectId, ref: 'Book'}] } // array goes here
         ]
     }
 )
