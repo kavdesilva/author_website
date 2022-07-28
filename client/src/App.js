5d3b766dc04f206950694fb00e539404d97640bc
Kristina <kavdesilva@gmail.com>
2022-07-28 14:04:36 -0400

    created react app

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import './styles/App.css';
+import React, { useState, useEffect } from 'react'
+import { Routes, Route } from 'react-router-dom'
+// import components below
+
+function App() {
+// functions go here
+
+  return (
+    <div className="App">
+      <header className="header">
+      </header>
+      <main>
+        <Routes>
+
+        </Routes>
+      </main>
+    </div>
+  );
+}
+
+export default App;
